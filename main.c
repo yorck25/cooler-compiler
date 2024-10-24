613ed35f0f5282b107201234150ec71de8c518c5
yorck25 <100796096+yorck25@users.noreply.github.com>
2024-10-24 22:40:10 +0200

    * adds todo and syscalls

--- main.c
+++ main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
+    char inputStream[] = "return 0;";
+
+    //TODO Jonas
+    // inputStream => array = ["return", "0", ";"]
+
     printf("Hello, World!\n");
     return 0;
 }
