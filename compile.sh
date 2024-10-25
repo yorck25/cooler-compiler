5fd69b2e4c0c15380b679ab1cb8c3ff5de2dc373
yorck25 <100796096+yorck25@users.noreply.github.com>
2024-10-26 01:25:11 +0200

    * adds script to compile the asm code

---
+++ compile.sh
@@ -0,0 +1,5 @@
+as -o ../output.o ../output.s
+
+ld -o ../output ../output.o
+
+echo "Successfully compiled"
--- main.c
+++ main.c
@@ -5,14 +5,14 @@
 
 int readFile(char **inputStream) {
     FILE *inputFile;
-    inputFile = fopen("input.txt", "r");
+    inputFile = fopen("../input.txt", "r");
 
     if (inputFile == NULL) {
         printf("Not able to open the file\n");
         return 1;
     }
 
-    *inputStream = (char *)malloc(100 * sizeof(char));
+    *inputStream = (char *) malloc(100 * sizeof(char));
     if (*inputStream == NULL) {
         printf("Memory allocation failed\n");
         fclose(inputFile);
@@ -30,6 +30,36 @@ int readFile(char **inputStream) {
     return 0;
 }
 
+int generator() {
+    FILE *fptr;
+
+    fptr = fopen("../output.s", "w");
+
+    fprintf(fptr, ".globl _main\n"
+                  ".align 2\n"
+                  "\n"
+                  "_main:\n"
+                  "    b _terminate\n"
+                  "\n"
+                  "_printf:\n"
+                  "    mov X0, #1 // stdout\n"
+                  "    adr X1, helloworld // address of string\n"
+                  "    mov X2, #12 // length of string\n"
+                  "    mov X16, #4 // write to stdout\n"
+                  "    svc 0    // syscall\n"
+                  "\n"
+                  "_terminate:\n"
+                  "    mov X0, #69 // return 0\n"
+                  "    mov X16, #1 // terminate\n"
+                  "    svc 0    // syscall\n"
+                  "\n"
+                  "helloworld: .asciz \"Hello, world\" \n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
 int main() {
     char *inputStream = NULL;
     char *parsedTokens[255];
@@ -42,14 +72,19 @@ int main() {
     printf("Content of the file: %s\n", inputStream);
 
     const int parsedCount = parse(&inputStream, parsedTokens);
-    if(parsedCount == 0){
+    if (parsedCount == 0) {
         printf("No tokens were passed back\n");
         return 1;
     }
 
-    for(int i = 0; i < parsedCount + 1; i++){
+    for (int i = 0; i < parsedCount + 1; i++) {
         printf("%s \n", parsedTokens[i]);
     }
+
+    generator();
+
+    system("chmod +x ../compile.sh");
+    system("../compile.sh");
 
     return 0;
 }
