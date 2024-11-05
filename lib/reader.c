ecc55db93e2f2ae1846a4e859722d045f3262720
yorck25 <100796096+yorck25@users.noreply.github.com>
2024-11-05 12:08:01 +0100

    * fixes that reader reads all lines

--- lib/reader.c
+++ lib/reader.c
@@ -3,28 +3,50 @@
 #include <stdlib.h>
 
 int readFile(char **inputStream) {
-    FILE *inputFile;
-    inputFile = fopen("../input.txt", "r");
+    int bufferSize = 100;
+    int lineBufferSize = 80;
 
+    FILE *inputFile = fopen("../input.txt", "r");
     if (inputFile == NULL) {
-        printf("Not able to open the file\n");
+        printf("Unable to open the file\n");
         return 1;
     }
 
-    *inputStream = (char *) malloc(100 * sizeof(char));
+    *inputStream = (char *)malloc(bufferSize * sizeof(char));
     if (*inputStream == NULL) {
         printf("Memory allocation failed\n");
         fclose(inputFile);
         return 1;
     }
+    (*inputStream)[0] = '\0';
 
-    if (fgets(*inputStream, 100, inputFile) == NULL) {
-        printf("Error reading from file\n");
-        fclose(inputFile);
+    char *lineBuffer = (char *)malloc(lineBufferSize * sizeof(char));
+    if (lineBuffer == NULL) {
+        printf("Memory allocation for line buffer failed\n");
         free(*inputStream);
+        fclose(inputFile);
         return 1;
     }
 
+    while (fgets(lineBuffer, lineBufferSize, inputFile) != NULL) {
+        if (strlen(*inputStream) + strlen(lineBuffer) + 1 > bufferSize) {
+            bufferSize *= 2;
+            char *temp = realloc(*inputStream, bufferSize * sizeof(char));
+            if (temp == NULL) {
+                printf("Memory reallocation failed\n");
+                free(*inputStream);
+                free(lineBuffer);
+                fclose(inputFile);
+                return 1;
+            }
+            *inputStream = temp;
+        }
+
+        strcat(*inputStream, lineBuffer);
+    }
+
+    free(lineBuffer);
     fclose(inputFile);
+
     return 0;
 }
